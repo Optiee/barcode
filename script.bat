e40571f3e2c652ed8fa731f4c32e35a22af70962
Optiee <kingdomsal3@hotmail.com>
2026-04-05 17:23:19 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,4 +1,5 @@
 :1
-if not exist %windir%\TSF2 (slmgr /dlv&start ms-settings:activation&exit)
-timeout /t 1>nul
-goto 1
+if exist %windir%\TSF2 (timeout /t 1>nul&goto 1)
+start "" slmgr /dlv
+start "" ms-settings:activation
+exit
